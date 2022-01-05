24e74b03ea2d072cf2bad9f7e9fb2103cc610a85
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-05 13:24:32 -0300

    tipos de dados basicos em Go: string, int, float, bool e error

--- variaveis/main.go
+++ variaveis/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	const nome string = "gabriel"
-	fmt.Printf(nome)
+	fmt.Println(nome)
+	const numero int8 = 40
+	fmt.Println(numero)
+	const numeroReal float32 = 12.4
+	fmt.Println(numeroReal)
+	char := 'B'
+	fmt.Println(char)
+	const boleano bool = true
+	fmt.Println(boleano)
+	var erro error = errors.New("Erro interno")
+	fmt.Println(erro)
+
 }
