5a99779fc1f742fd3e967a52e0c776ad9fe4eeaa
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-08 17:18:53 -0300

    feature(buscar servidores): criando função de buscar por servidores

--- Mini-App-CLI/app/app.go
+++ Mini-App-CLI/app/app.go
@@ -12,17 +12,26 @@ func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca por ips e servidores na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "www.atoscapital.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca por ips na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "www.atoscapital.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca por ips na internet",
+			Flags:  flags,
 			Action: buscarPorIp,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca por Servidores na internet",
+			Flags:  flags,
+			Action: buscarPorServidores,
 		},
 	}
 	return app
@@ -37,5 +46,17 @@ func buscarPorIp(c *cli.Context) {
 	for _, ip := range ips {
 
 		fmt.Println(ip)
+	}
+}
+
+func buscarPorServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, erro := net.LookupNS(host) // ns Nome servidor:
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, servidor := range servidores {
+
+		fmt.Println(servidor.Host)
 	}
 }
